696fcd0bf9b491cd7ad5990114fde428952ee6be
gdm413229 <raemochrie@gmail.com>
2019-08-22 13:36:01 +0100

    Added DevIL src as git submodule.  Windows builds use existing binaries and Linux ones build DevIL from src prior to building the Builder.

---
+++ Source/Core/Rendering/GLMesh.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using OpenGL;
+namespace CodeImp.DoomBuilder.Rendering
+{
+    internal class GLMesh : IDisposable
+    {
+
+
+        public GLMesh(GLContext context,uint indexcnt,uint vertexcnt,,GLElementBuffer elements)
+        {
+
+        }
+
+        public void Dispose()
+        {
+            GC.Collect(); // whip those bin men!
+        }
+    }
+
+    internal class GLVertexElement
+    {
+        public uint vertex_id;
+
+    }
+
+    internal class GLVertexBuffer
+    {
+        public uint num_elements;
+        GLVertexElement[] elements;
+
+        public GLVertexBuffer(uint num_elems,uint vert_id)
+        {
+            this.num_elements = num_elems;
+            uint cur_elem = 0;
+            while(cur_elem < num_elems)
+            {
+
+                cur_elem++;
+            }
+        }
+
+    }
+
+}
+
